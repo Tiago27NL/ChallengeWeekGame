ac9eeeaad23b1bd846fb2dba5902e4ab8c1fabbe
Milan <1093778@hr.nl>
2024-12-09 19:09:05 +0100

    Update top_down_shooter.py

--- top_down_shooter.py
+++ top_down_shooter.py
@@ -9,17 +9,27 @@ pygame.init()
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("Top Down Shooter")  # Naam van het spel (kunnen we nog veranderen)
 clock = pygame.time.Clock() 
-
 #load images
 background = pygame.transform.scale(pygame.image.load("background/background.jpg").convert(), (WIDTH, HEIGHT))
 
 class Player(pygame.sprite.Sprite):
     def __init__(self):
         super().__init__()
-        self.image = pygame.transform.rotozoom(pygame.image.load("player/player.png").convert_alpha(),0, PLAYER_SIZE)
         self.pos = pygame.math.Vector2(PLAYER_START_X, PLAYER_START_Y)
+        self.image = pygame.transform.rotozoom(pygame.image.load("player/player.png").convert_alpha(),0, PLAYER_SIZE)
+        self.base_player_image = self.image
+        self.hitbox_rect = self.base_player_image.get_rect(center = self.pos)
+        self.rect = self.hitbox_rect.copy()
         self.speed = PLAYER_SPEED
-    
+        
+    def player_rotation(self):
+        self.mouse_coords = pygame.mouse.get_pos()
+        self.x_change_mouse_player = (self.mouse_coords[0] - self.hitbox_rect.centerx)
+        self.y_change_mouse_player = (self.mouse_coords[1] - self.hitbox_rect.centery)
+        self.angle = math.degrees(math.atan2(self.y_change_mouse_player, self.x_change_mouse_player))
+        self.image = pygame.transform.rotate(self.base_player_image, int(-self.angle))
+        self.rect = self.image.get_rect(center = self.hitbox_rect.center)
+
     def user_input(self):
         self.velocity_x = 0
         self.velocity_y = 0
@@ -40,11 +50,13 @@ class Player(pygame.sprite.Sprite):
 
     def move(self):
         self.pos += pygame.math.Vector2(self.velocity_x, self.velocity_y)
+        self.hitbox_rect.center = self.pos
+        self.rect.center = self.hitbox_rect.center
 
     def update(self):
         self.user_input()
         self.move()
-
+        self.player_rotation()
 
 player = Player()
 
@@ -58,8 +70,11 @@ while True:
 
 
     screen.blit(background, (0, 0))
-    screen.blit(player.image, player.pos)
+    screen.blit(player.image, player.rect)
     player.update()
+    pygame.draw.rect(screen, "red", player.hitbox_rect, width = 2)
+    pygame.draw.rect(screen, "yellow", player.rect, width = 2)
+
 
     pygame.display.update()
     clock.tick(FPS)
