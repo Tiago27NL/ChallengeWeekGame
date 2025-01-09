5508e8d43639cd018acfca97455543c39696593d
Milan <1093778@hr.nl>
2025-01-09 11:33:33 +0100

    Update top_down_shooter.py

--- top_down_shooter.py
+++ top_down_shooter.py
@@ -9,8 +9,9 @@ pygame.init()
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("Top Down Shooter")  # Naam van het spel (kunnen we nog veranderen)
 clock = pygame.time.Clock() 
-#load images
-background = pygame.transform.scale(pygame.image.load("background/background.jpg").convert(), (WIDTH, HEIGHT))
+# load images
+# background = pygame.transform.scale(pygame.image.load("background/background.jpg").convert() , (WIDTH, HEIGHT))
+background = pygame.image.load("background/ground.png").convert()
 
 class Player(pygame.sprite.Sprite):
     def __init__(self):
@@ -21,11 +22,14 @@ class Player(pygame.sprite.Sprite):
         self.hitbox_rect = self.base_player_image.get_rect(center = self.pos)
         self.rect = self.hitbox_rect.copy()
         self.speed = PLAYER_SPEED
+        self.shoot = False
+        self.shoot_cooldown = 0
+        self.gun_barrel_offset = pygame.math.Vector2(GUN_OFFSET_X, GUN_OFFSET_Y)
         
     def player_rotation(self):
         self.mouse_coords = pygame.mouse.get_pos()
-        self.x_change_mouse_player = (self.mouse_coords[0] - self.hitbox_rect.centerx)
-        self.y_change_mouse_player = (self.mouse_coords[1] - self.hitbox_rect.centery)
+        self.x_change_mouse_player = (self.mouse_coords[0] - WIDTH // 2)
+        self.y_change_mouse_player = (self.mouse_coords[1] - HEIGHT // 2)
         self.angle = math.degrees(math.atan2(self.y_change_mouse_player, self.x_change_mouse_player))
         self.image = pygame.transform.rotate(self.base_player_image, int(-self.angle))
         self.rect = self.image.get_rect(center = self.hitbox_rect.center)
@@ -48,6 +52,22 @@ class Player(pygame.sprite.Sprite):
             self.velocity_x /= math.sqrt(2)
             self.velocity_y /= math.sqrt(2)
 
+        if pygame.mouse.get_pressed() == (1, 0, 0) or keys[pygame.K_SPACE]:
+            self.shoot = True
+            self.is_shooting()
+        else:
+            self.shoot = False
+    
+    def is_shooting(self):
+        if self.shoot_cooldown == 0:
+            self.shoot_cooldown = SHOOT_COOLDOWN
+            spawn_bullet_pos = self.pos + self.gun_barrel_offset.rotate(self.angle)
+            self.bullet = Bullet(spawn_bullet_pos[0], spawn_bullet_pos[1], self.angle)
+            bullet_group.add(self.bullet)
+            all_sprites_group.add(self.bullet)
+            
+
+
     def move(self):
         self.pos += pygame.math.Vector2(self.velocity_x, self.velocity_y)
         self.hitbox_rect.center = self.pos
@@ -58,8 +78,103 @@ class Player(pygame.sprite.Sprite):
         self.move()
         self.player_rotation()
 
-player = Player()
+        if self.shoot_cooldown > 0:
+            self.shoot_cooldown -= 1
 
+class Bullet(pygame.sprite.Sprite):
+    def __init__(self, x ,y, angle):
+        super().__init__()
+        self.image = pygame.image.load("bullet/bullet.png").convert_alpha()
+        self.image = pygame.transform.rotozoom(self.image, 0, BULLET_SCALE)
+        self.rect = self.image.get_rect()
+        self.rect.center = (x, y)
+        self.x = x
+        self.y = y
+        self.angle = angle
+        self.speed = BULLET_SPEED
+        self.x_velocity = math.cos(self.angle * (2 * math.pi / 360)) * self.speed
+        self.y_velocity = math.sin(self.angle * (2 * math.pi / 360)) * self.speed
+        self.bullet_lifetime = BULLET_LIFETIME
+        self.spawn_time = pygame.time.get_ticks() #gets the specific time the bullet was created
+
+    def bullet_movement(self):
+        self.x += self.x_velocity
+        self.y += self.y_velocity
+
+        self.rect.x = int(self.x) #float naar int
+        self.rect.y = int(self.y)
+
+        if pygame.time.get_ticks() - self.spawn_time > self.bullet_lifetime:
+            self.kill() # delete bullet
+
+    def update(self):
+        self.bullet_movement()
+
+class Enemy(pygame.sprite.Sprite):
+    def __init__(self, position):
+        super().__init__(enemy_group, all_sprites_group)
+        self.image = pygame.image.load("enemy/enemy.png").convert_alpha()
+        self.image = pygame.transform.rotozoom(self.image, 0, ENEMY_SCALE)
+
+        self.rect = self.image.get_rect()
+        self.rect.center = position
+
+        self.position = pygame.math.Vector2(position)
+        self.direction = pygame.math.Vector2()
+        self.velocity = pygame.math.Vector2()
+        self.speed = ENEMY_SPEED
+
+    def hunt_player(self):
+        player_vector = pygame.math.Vector2(player.hitbox_rect.center)
+        enemy_vector = pygame.math.Vector2(self.rect.center)
+        distance = self.get_vector_distance(player_vector, enemy_vector)
+
+        if distance > 0:
+            self.direction = (player_vector - enemy_vector).normalize()
+        else:
+            self.direction = pygame.math.Vector2()
+
+        self.velocity = self.direction * self.speed
+        self.position += self.velocity
+
+        self.rect.centerx = self.position.x
+        self.rect.centery = self.position.y
+
+    def update(self):
+        self.hunt_player()
+
+    def get_vector_distance(self, vector_1, vector_2):
+        return (vector_1 - vector_2).magnitude()
+
+
+class Camara(pygame.sprite.Group):
+    def __init__(self):
+        super().__init__()
+        self.offset = pygame.math.Vector2()
+        self.floor_rect = background.get_rect(topleft = (0, 0))
+
+    def custom_draw(self): 
+        self.offset.x = player.rect.centerx - (WIDTH // 2) # gotta blit the player rect not base rect
+        self.offset.y = player.rect.centery - (HEIGHT // 2)
+
+        # draw the floor
+        floor_offset_pos = self.floor_rect.topleft - self.offset
+        screen.blit(background, floor_offset_pos)
+
+        for sprite in all_sprites_group: 
+            offset_pos = sprite.rect.topleft - self.offset
+            screen.blit(sprite.image, offset_pos)
+
+all_sprites_group = pygame.sprite.Group()
+bullet_group = pygame.sprite.Group()
+enemy_group = pygame.sprite.Group()
+
+camera = Camara()
+player = Player()
+enemy = Enemy((500, 0))
+
+
+all_sprites_group.add(player)
 
 while True:
     keys = pygame.key.get_pressed()
@@ -70,12 +185,12 @@ while True:
 
 
     screen.blit(background, (0, 0))
-    screen.blit(player.image, player.rect)
-    player.update()
-    pygame.draw.rect(screen, "red", player.hitbox_rect, width = 2)
-    pygame.draw.rect(screen, "yellow", player.rect, width = 2)
+    
+    camera.custom_draw()
+    all_sprites_group.update()
+    # pygame.draw.rect(screen, "red", player.hitbox_rect, width = 2)
+    # pygame.draw.rect(screen, "yellow", player.rect, width = 2)
 
 
     pygame.display.update()
     clock.tick(FPS)
-
